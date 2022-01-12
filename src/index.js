491f93535d0a00038e46e2e11c608925f8dbebd3
gopivaibhav <gopivaibhav0@gmail.com>
2022-01-12 12:19:00 +0530

    Login with OAuth added

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+import Login from './Components/Login'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Login />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
