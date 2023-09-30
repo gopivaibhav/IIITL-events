fc4b11bd323d4d1b4200f31469c1fc78674f47fd
Gopi Vaibhav <gopivaibhav0@gmail.com>
2023-09-30 20:49:06 +0530

    Added dockerfiles

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -45,56 +45,58 @@ export default function Home() {
 
   return (
     <>
-      <div className="Q-A">
-        <p>Which Game are you most intrested in ? </p>
-        <p>(Prefer college IDs to vote) </p>
-        <div onChange={onChangeValue} className="options">
-          <p>
-            <input type="radio" value="Valorant" name="game" id="valo" />
-            <label htmlFor="valo">Valorant</label>{" "}
-          </p>
-          <p>
-            <input type="radio" value="CSGO" name="game" id="csgo" />
-            <label htmlFor="csgo"> CSGO </label>
-          </p>
-          <p>
-            <input
-              type="radio"
-              value="Call of Duty"
-              name="game"
-              id="Call of Duty"
-            />
-            <label htmlFor="Call of Duty"> Call of Duty </label>
-          </p>
-          <p>
-            <input type="radio" value="BGMI" name="game" id="bgmi" />
-            <label htmlFor="bgmi"> BGMI </label>
-          </p>
-          <p>
-            <input type="radio" value="Other" name="game" id="other" />
-            <label htmlFor="other"> Other</label>{" "}
-          </p>
+      <div className="home">
+        <div className="Q-A">
+          <p>Which Game are you most intrested in ? </p>
+          <p>(Prefer college IDs to vote) </p>
+          <div onChange={onChangeValue} className="options">
+            <p>
+              <input type="radio" value="Valorant" name="game" id="valo" />
+              <label htmlFor="valo">Valorant</label>{" "}
+            </p>
+            <p>
+              <input type="radio" value="CSGO" name="game" id="csgo" />
+              <label htmlFor="csgo"> CSGO </label>
+            </p>
+            <p>
+              <input
+                type="radio"
+                value="Call of Duty"
+                name="game"
+                id="Call of Duty"
+              />
+              <label htmlFor="Call of Duty"> Call of Duty </label>
+            </p>
+            <p>
+              <input type="radio" value="BGMI" name="game" id="bgmi" />
+              <label htmlFor="bgmi"> BGMI </label>
+            </p>
+            <p>
+              <input type="radio" value="Other" name="game" id="other" />
+              <label htmlFor="other"> Other</label>{" "}
+            </p>
+          </div>
+          {other && (
+            <p>
+              <input
+                type="text"
+                className="text"
+                onChange={(e) => setValue(e.target.value)}
+                placeholder="Write Game name"
+              />
+            </p>
+          )}
+          <button onClick={submitForm}>
+            Submit
+            <span className="first"></span>
+            <span className="second"></span>
+            <span className="third"></span>
+            <span className="fourth"></span>
+          </button>
+          {error && <p className="error">{error}</p>}
         </div>
-        {other && (
-          <p>
-            <input
-              type="text"
-              className="text"
-              onChange={(e) => setValue(e.target.value)}
-              placeholder="Write Game name"
-            />
-          </p>
-        )}
-        <button onClick={submitForm}>
-          Submit
-          <span className="first"></span>
-          <span className="second"></span>
-          <span className="third"></span>
-          <span className="fourth"></span>
-        </button>
-        {error && <p className="error">{error}</p>}
+        <Feed />
       </div>
-      <Feed />
     </>
   );
 }
